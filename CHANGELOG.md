a368139f3a2e54a2b2960d4ff771a17a9e8be8d1
Mensch <mensch272@users.noreply.github.com>
2022-08-25 11:36:39 +0500

    feat: base facet structure and minimal docs.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 # 0.1.0+1
 
-- TODO: Describe initial release.
+- First release with basic facet structure.
---
+++ __brick__/{{name.snakeCase()}}/{{name.snakeCase()}}.dart
@@ -0,0 +1,7 @@
+export 'models/models.dart';
+export 'providers/providers.dart';
+export 'services/services.dart';
+export 'widgets/widgets.dart';
+
+// Views.
+export 'views/{{name.snakeCase()}}_page.dart';
